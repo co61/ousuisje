af3bb922609d7d43a33286a4d31df72a57d357b8
Corentin Régné <corentin.regne@gmail.com>
2020-12-08 10:24:09 +0100

    first commit

---
+++ app/src/main/java/fr/c7regne/ousuisje/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.c7regne.ousuisje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
